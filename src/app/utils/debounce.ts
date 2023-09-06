d7e2a4f7e80d9c82f9eb10b1096e9dcb88d561f4
Asier <laraasier@gmail.com>
2023-09-06 23:37:00 +0200

    Change any for NodeJS.Timeout

--- src/app/utils/debounce.ts
+++ src/app/utils/debounce.ts
@@ -2,16 +2,11 @@ const debounce = <T extends (...args: any[]) => void>(
   func: T,
   delay: number
 ): ((...args: Parameters<T>) => void) => {
-  let timer: any;
+  let timer: NodeJS.Timeout;
 
   return (...args: Parameters<T>) => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      func(...args);
-      timer = undefined;
-    }, delay);
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
   };
 };
 
